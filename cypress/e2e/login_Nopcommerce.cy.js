f658ddb7d553c1a17f8dc23353febe53ff7e8e59
Nimithaea <Nimithaea@gmail.com>
2024-09-10 07:00:07 +0200

    updated login test

--- cypress/e2e/login_Nopcommerce.cy.js
+++ cypress/e2e/login_Nopcommerce.cy.js
@@ -7,16 +7,22 @@ describe('Nopcommerce Login', ()=>{
 
       const login = new LoginPage()
 
-        cy.visit('http://nimithatest-001-site1.ctempurl.com', {
+        cy.visit('http://nimithatest-001-site1.ctempurl.com/admin', {
             auth: {
               username: '11190286',
               password: '60-dayfreetrial',
             }
             })
+
+            cy.get('.header-links').contains("Log in").click()
+            cy.get('.email').type('nimithaea@gmail.com')
+            cy.get('#Password').type('nimithacypress')
+            cy.get('.master-wrapper-page').find('.button-1 login-button').click()
+            //cy.get('.buttons').click()//cy.get('body')
             //login.visit()
-            login.Username('admin@yourstore.com')
-            login.Password('admin')
-            login.LogIn()
+            //login.Username('admin@yourstore.com')
+            //login.Password('admin')
+            //login.LogIn()
         
     })
 })
